114a45bd8612e0329231885dace26edcc4faa222
Rahul Ugale <rugale980@gmail.com>
2025-11-21 23:52:31 +0530

    Add files via upload

---
+++ Assignments/Assignment_30/program30_5.c
@@ -0,0 +1,78 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header file
+//
+////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description   : Accept number of row and  number of column from user and
+//                  display below pattern.
+//  Input         : Interger
+//  Output        : Integer
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int iCnt = 0;
+    int jCnt = 0;
+
+    for (iCnt = 1; iCnt <= iRow; iCnt++)
+    {
+        for (jCnt = 1; jCnt <= iCol; jCnt++)
+        {
+            for (jCnt = 1; jCnt <= iCol; jCnt++)
+            {
+                if (iCnt == 1 || iCnt == iRow || jCnt == 1 || jCnt == iCol)
+                {
+                    printf("%d\t", jCnt);
+                }
+                else
+                {
+                    printf("*\t", jCnt);
+                }
+            }
+            printf("\n");
+        }
+    }
+} // End of function
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of Columns : \n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+} // End of main
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handle by the application
+//
+//  Input  : iRow = 4 , iCol = 4 :
+//
+//  Output : 1       2       3       4
+//           1       *       *       4
+//           1       *       *       4
+//           1       2       3       4
+//
+////////////////////////////////////////////////////////////////////////////////
