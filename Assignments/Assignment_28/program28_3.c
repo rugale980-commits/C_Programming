b009b6f30dea532844e7b5acace3856158ee4bde
Rahul Ugale <rugale980@gmail.com>
2025-11-21 23:53:44 +0530

    Add files via upload

---
+++ Assignments/Assignment_28/program28_3.c
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header file
+//
+////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description   : Accept number of row and  number of column from user and
+//                  display below pattern.
+//  Input         : Interger
+//  Output        : Integer
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 20/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int iCnt = 0;
+    int jCnt = 0;
+    char ch = 'A';
+
+    for (iCnt = 1; iCnt <= iRow; iCnt++)
+    {
+        for (jCnt = 1; jCnt <= iCol; jCnt++)
+        {
+            printf("%c\t", ch);
+        }
+        printf("\n");
+        ch++;
+    }
+} // End of function
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of Columns : \n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+} // End of main
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handle by the application
+//
+//  Input  : iRow = 3 , iCol = 5 :
+//
+//  Output :A       A       A       A       A
+//          B       B       B       B       B
+//          C       C       C       C       C
+//
+////////////////////////////////////////////////////////////////////////////////
