09dd524553176dd0fc7442e7eb6b36bd5c6ddc17
Rahul Ugale <rugale980@gmail.com>
2025-11-28 14:26:29 +0530

    Remove unused variable in ChkAlpha function
    Removed unused variable 'iCnt' from ChkAlpha function.

--- Assignments/Assignment_33/program33_1.c
+++ Assignments/Assignment_33/program33_1.c
@@ -25,8 +25,6 @@ typedef int BOOL;
 
 BOOL ChkAlpha(char str[])
 {
-    int iCnt = 0;
-
     while (*str != '\0')
     {
         if ((*str >= 'A') && (*str <= 'Z') || (*str >= 'a') && (*str <= 'z'))
