5bccfea489131902b53df4e3a80ffee28508134c
Rahul Ugale <rugale980@gmail.com>
2025-11-29 09:20:03 +0530

    Add files via upload

---
+++ Assignments/Assignment_33/program33_5.c
@@ -0,0 +1,78 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required header file
+//
+////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : ChkAlpha
+//  Description   : Accept division of student from user & depends on the division
+//                  display Exam timing
+//  Input         : Character
+//  Output        : Character
+//  Author        : Rahul Balasaheb Ugale
+//  Date          : 28/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+void DisplaySchedule(char chDiv[])
+{
+    switch (*chDiv)
+    {
+    case 'A':
+    case 'a':
+        printf("Your exam at 7 AM");
+        break;
+
+    case 'B':
+    case 'b':
+        printf("Your exam at 8.30 AM");
+        break;
+
+    case 'C':
+    case 'c':
+        printf("Your exam at 9.20 AM");
+        break;
+
+    case 'D':
+    case 'd':
+        printf("Your exam at 10.30 AM");
+        break;
+
+    default:
+        printf("Invalid division");
+    }
+    chDiv++;
+} // End of function
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char cValue[] = {'\0'};
+
+    printf("Enter the character :\n");
+    scanf("%c", &cValue);
+
+    DisplaySchedule(cValue);
+
+    return 0;
+} // End of main
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handle by the application
+//
+//  Input  : C
+//  Output : Your exam at 9.20 Am
+//
+//  Input  : d
+//  Output : Your exam at 10.30 AM
+//
+////////////////////////////////////////////////////////////////////////////////
